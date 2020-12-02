0ae4c4ce460135351239c451e7daaac67dcaf6b0
Prashastha01 <kovuriprashastha01@gmail.com>
2020-12-02 17:59:21 +0530

    service created

---
+++ Question-3/myfirstappln/myfirstappln/index.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="myfirstappln.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox3" placeholder="Enter ID" runat="server"></asp:TextBox>
+        </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Get Username" />
+        <p>
+            <asp:Label ID="greet" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Question-3/myfirstappln/myfirstappln/index.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using myfirstappln.ServiceReference1;
+using myfirstappln.ServiceReference3;
+namespace myfirstappln
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String enteredid = TextBox3.Text;
+            Service1Client client = new Service1Client();
+            String name = client.username(enteredid);
+            greet.Text = name;
+        }
+    }
+}
